e3d9acef96b76bdce6ac74387543a72465dabf1f
ittiphol <ittiphol.puangchaiya@gmail.com>
2024-03-05 09:56:09 +0700

    01-Create-Next-App-Layout

---
+++ components/header/header.tsx
@@ -0,0 +1,29 @@
+
+import Link from '@/node_modules/next/link'
+import React from 'react'
+
+const Header = () => {
+  return <header>
+      <nav>
+          <div className="navbar justify-between bg-base-300">
+              <Link href="/" className="btn btn-ghost text-lg">
+                  Next Amazona V2
+              </Link>
+              <ul className="flex">
+                <li>
+                    <Link className="btn btn-ghost rounded-btn" href="/cart">
+                        Cart
+                    </Link>
+                </li>
+                <li>
+                    <Link className="btn btn-ghost rounded-btn" href="/signin">
+                        Sign in
+                    </Link>
+                </li>
+              </ul>
+          </div>
+      </nav>
+  </header>
+}
+
+export default Header
